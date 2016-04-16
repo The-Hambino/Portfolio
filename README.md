0a14a05f8b56ac4bd6d93fa6d34fe9e2d9f6465c
Clint <Clint>
2016-04-16 02:21:43 -0700

    Final updates. Pushed to live server for testing.

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
 # Portfolio 2016
-Basic Hierarchy
-
-Index.html
-- About-Clint-Naugle.html
-- Resume-Clint-Naugle.html
-- RWD-01.html
-  - html-mocks/responsive-template/index.html
-- RWD-02.html
-  - html-mocks/windows-10/index.html
-- Ecommerce-01.html
-- Print-01.html
+This is the web design portfolio for Clint Naugle.
--- js/base.js
+++ js/base.js
@@ -7,3 +7,51 @@ $("#resume").click(function() {
 		$("#resume").removeClass("show-resume").addClass("hide-resume");
 	};
 });  
+
+$("#rwd-link").click(function() {
+	if ($("#rwd-content").hasClass("hide-content")) {
+		$("#portfolio-content").addClass("portfolio-open");
+		$("#ecommerce-link").removeClass("link-open");
+		$("#print-link").removeClass("link-open");
+		$("#ecommerce-content").removeClass("show-content").addClass("hide-content");
+		$("#print-content").removeClass("show-content").addClass("hide-content");
+		$("#rwd-content").addClass("show-content").removeClass("hide-content");
+		$("#rwd-link").addClass("link-open");
+	} else {
+		$("#rwd-content").removeClass("show-content").addClass("hide-content");
+		$("#rwd-link").removeClass("link-open");
+		$("#portfolio-content").removeClass("portfolio-open");
+	};
+});
+
+$("#ecommerce-link").click(function() {
+	if ($("#ecommerce-content").hasClass("hide-content")) {
+		$("#portfolio-content").addClass("portfolio-open");
+		$("#rwd-link").removeClass("link-open");
+		$("#print-link").removeClass("link-open");
+		$("#rwd-content").removeClass("show-content").addClass("hide-content");
+		$("#print-content").removeClass("show-content").addClass("hide-content");
+		$("#ecommerce-content").addClass("show-content").removeClass("hide-content");
+		$("#ecommerce-link").addClass("link-open");
+	} else {
+		$("#ecommerce-content").removeClass("show-content").addClass("hide-content");
+		$("#ecommerce-link").removeClass("link-open");
+		$("#portfolio-content").removeClass("portfolio-open");
+	};
+});
+
+$("#print-link").click(function() {
+	if ($("#print-content").hasClass("hide-content")) {
+		$("#portfolio-content").addClass("portfolio-open");
+		$("#rwd-link").removeClass("link-open");
+		$("#ecommerce-link").removeClass("link-open");
+		$("#rwd-content").removeClass("show-content").addClass("hide-content");
+		$("#ecommerce-content").removeClass("show-content").addClass("hide-content");
+		$("#print-content").addClass("show-content").removeClass("hide-content");
+		$("#print-link").addClass("link-open");
+	} else {
+		$("#print-content").removeClass("show-content").addClass("hide-content");
+		$("#print-link").removeClass("link-open");
+		$("#portfolio-content").removeClass("portfolio-open");
+	};
+});
